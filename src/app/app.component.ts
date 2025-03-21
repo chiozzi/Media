3f3ac7935840c39912d71bfb813faa3bdbc90475
chiozzi <chiozziletici0114@gmail.com>
2025-03-21 10:40:33 -0300

    Primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Media';
+
+  num1: number | null = null;
+  num2: number | null = null;
+  num3: number | null = null;
+  num4: number | null = null;
+  media: number | null = null;
+
+  calcularmedia(){
+    if (this.num1 !== null && this.num2 !== null && this.num3 !== null && this.num4 !== null) {
+      this.media = (this.num1 + this.num2 + this.num3 + this.num4) / 4
+
+   }
+  }
 }
+
