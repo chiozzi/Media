56cb2977f096468cdf7c5d8d018a7734208e4bc7
chiozzi <chiozziletici0114@gmail.com>
2025-03-21 11:01:13 -0300

    atualização

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,24 @@
-import { Component } from '@angular/core';
+  import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  standalone: false,
-  styleUrl: './app.component.css'
-})
-export class AppComponent {
-  title = 'Media';
+  @Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    standalone: false,
+    styleUrl: './app.component.css'
+  })
+  export class AppComponent {
+    title = 'Media';
 
-  num1: number | null = null;
-  num2: number | null = null;
-  num3: number | null = null;
-  num4: number | null = null;
-  media: number | null = null;
+    num1: number | null = null;
+    num2: number | null = null;
+    num3: number | null = null;
+    num4: number | null = null;
+    media: number | null = null;
 
-  calcularmedia(){
-    if (this.num1 !== null && this.num2 !== null && this.num3 !== null && this.num4 !== null) {
-      this.media = (this.num1 + this.num2 + this.num3 + this.num4) / 4
-   }
+    calcularmedia(){
+      if (this.num1 !== null && this.num2 !== null && this.num3 !== null && this.num4 !== null) {
+        this.media = (this.num1 + this.num2 + this.num3 + this.num4) / 4
+    }
+    }
   }
-}
 
